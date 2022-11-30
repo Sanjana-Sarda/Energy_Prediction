974a6281f67095ec686810fbfc3e0e6bc690a49c
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2022-11-29 21:59:02 -0800

    temp

--- master_worker.py
+++ master_worker.py
@@ -2,4 +2,7 @@ from kubernetes import client, config
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
-print (v1.list_node())
+# v1.list_node() - kubectl get nodes
+# v1.list_namespace() - kubectl get namespaces
+#v1.list_pod_for_all_namespaces()
+#v1.list_persistent_volume_claim_for_all_namespaces()
