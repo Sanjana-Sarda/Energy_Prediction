89436ad66af87e8934234990b8e4fcda17c13342
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2022-12-02 16:21:36 -0800

    bytearray

--- container/inference.py
+++ container/inference.py
@@ -3,10 +3,12 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error 
 import numpy as np
+import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish 
 import paho.mqtt.subscribe as subscribe
 import tensorflow as tf
 from keras.optimizers import Adam
+import pickle
 
 
 def train(model, X_train, y_train):
@@ -20,9 +22,26 @@ def finetune(model, X_train, y_train):
         model.compile(optimizer=Adam(1e-2), loss='mean_absolute_error', metrics=['mean_absolute_error'])
         model.fit(X_train, y_train, epochs=5, batch_size=32, validation_split = 0.2)
 
+#def on_connect(client, userdata, msg):
+ #   client.subscribe("Global_Model")
+
+#def on_message (client, userdata, msg):
+ #   global weights, model_received
+  #  weights = deserialize(userdata)
+   # model_received = True
+    
+def serialize(model):
+    return pickle.dumps({"weights": model.get_weights()})
+
+def deserialize(nn_bytes):
+    loaded = pickle.loads(nn_bytes)
+    weights = loaded['weights']
+    return weights
+
 num_points = 336
 count = 0
 all_inf = []
+model_received = False
 
 mqttBroker = "100.90.105.93"
 
@@ -36,6 +55,11 @@ X = data.drop(columns=['Total Energy (kWh)', 'Total Solar Energy (kWh)'])
 scaler = StandardScaler()
 X_week = np.zeros([num_points, X.shape[1]])
 y_week = np.zeros([num_points, y.shape[1]])
+
+#client = mqtt.Client("House_a")
+#client.on_connect = on_connect
+#client.on_message = on_message
+
 
 for idx, X_row, in X.iterrows():
     X_row = np.array(X_row).reshape((1, 1450))
@@ -51,15 +75,18 @@ for idx, X_row, in X.iterrows():
         pre_mae = mean_absolute_error(NN_model(X_week), y_week)
         NN_model = train(NN_model, X_week, y_week)
         post_mae = mean_absolute_error(NN_model(X_week), y_week)
-        #publish.single("House/a", [NN_model, pre_mae, post_mae], hostname = mqttBroker) # send mae before and after
-        publish.single("House/a",  pre_mae, hostname = mqttBroker) # send mae before and after
-        #updated_model = subscribe.simple("Gobal_Model", hostname =mqttBroker, keepalive=60)
-        #updated_model = finetune(updated_model, X_week, y_week)
-        #updated_mae = mean_absolute_error(updated_model(X_week), y_week)
-        #if (post_mae<=updated_mae):
-         #   NN_model = tf.keras.models.clone_model(updated_model)
-        #X_week = np.zeros([num_points, X.shape[1]])
-        #y_week = np.zeros([num_points, y.shape[1]])
+        publish.single("House/a", serialize(NN_model), hostname = mqttBroker) 
+        publish.single("House/a/pre_mae",  pre_mae, hostname = mqttBroker) 
+        publish.single("House/a/post_mae",  post_mae, hostname = mqttBroker) 
+        weights = deserialize(subscribe.simple("Global_Model", hostname =mqttBroker, keepalive=60))
+        updated_model = tf.keras.models.clone_model(NN_model)
+        updated_model.set_weights(weights)
+        updated_model = finetune(updated_model, X_week, y_week)
+        updated_mae = mean_absolute_error(updated_model(X_week), y_week)
+        if (post_mae<=updated_mae):
+            NN_model = tf.keras.models.clone_model(updated_model)
+        X_week = np.zeros([num_points, X.shape[1]])
+        y_week = np.zeros([num_points, y.shape[1]])
         count = 0 
         
         
