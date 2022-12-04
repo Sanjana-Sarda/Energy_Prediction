c9feda6d2b14eda6bad77a509b1bd88fde1b89f9
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2022-12-04 15:02:27 -0800

    scaling issue

--- container/inference.py
+++ container/inference.py
@@ -1,6 +1,7 @@
 from keras.models import load_model
 import pandas as pd
 from sklearn.metrics import mean_absolute_error 
+from sklearn.metrics import StandardScaler 
 import numpy as np
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish 
@@ -62,6 +63,7 @@ data = pd.read_csv('test_data.csv', index_col=0)
 data = data.reset_index(drop=True)
 scaler_mean = pd.read_csv('mean_orig.csv').iloc[:, 1].to_numpy().reshape((1, 1450))
 scaler_var = pd.read_csv('var_orig.csv').iloc[:, 1].to_numpy().reshape((1, 1450))
+scaler = StandardScaler()
 data = data.drop(columns=['dataid'])
 
 y = data[['Total Energy (kWh)', 'Total Solar Energy (kWh)']]
@@ -71,15 +73,16 @@ y_week = np.zeros([num_points, y.shape[1]])
 
 for idx, X_row, in X.iterrows():
     X_row = np.array(X_row).reshape((1, 1450))
+    X_week[count] = X_row
+    y_week[count] = y_row
     X_row = (X_row - scaler_mean)/np.sqrt(scaler_var)
     y_row = np.array(y.iloc[idx])
     inf = NN_model(X_row)
     all_inf.append(inf)
-    X_week[count] = X_row
-    y_week[count] = y_row
     
     count +=1
     if (count == num_points):
+        X_week = scaler.fit_transform(X_week)
         pre_mae = mean_absolute_error(NN_model(X_week), y_week)
         print ("pre_mae ="+ str(pre_mae))
         trained_model = tf.keras.models.clone_model(NN_model)
@@ -109,8 +112,8 @@ for idx, X_row, in X.iterrows():
         print ("updated_mae" + str(updated_mae))
         if (updated_mae<=post_mae):
             NN_model.set_weights(updated_model.get_weights())
-        scaler_mean = np.mean(X_week, axis=0).reshape((1, 1450))
-        scalar_var = np.var(X_week, axis=0).reshape((1, 1450))
+        scaler_mean = scaler.mean_
+        scalar_var = scaler.var_
         X_week = np.zeros([num_points, X.shape[1]])
         y_week = np.zeros([num_points, y.shape[1]])
         count = 0 
