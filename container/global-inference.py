6cd3962a8a91d39128beb563f0bda7c192e02057
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2022-12-04 18:24:11 -0800

    remove logs

--- container/global-inference.py
+++ container/global-inference.py
@@ -32,9 +32,7 @@ def get_house_model(client, userdata, msg):
 
 def get_inf(client, userdata, msg):
     global inf, min
-    print (msg.payload.decode("utf-8"))
     inf[msg.topic[-1]].append(msg.payload.decode("utf-8"))
-    print (inf[msg.topic[-1]])
     if (len(inf[msg.topic[-1]])==min_val):
         df=pd.DataFrame.from_dict(inf,orient='index').transpose()
         df.to_csv("inf.csv")
